a85c6f0d919f5c964719a66fbef35d64c72f90ca
Isaiah <114616212+izzaiah11@users.noreply.github.com>
2023-01-31 10:25:49 -0800

    updated files

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+/*
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist */ 
+
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(){
+        var value = $("this").siblings(".description").val();
+        var time = $("this").parent().attr("id");
+        localStorage.setItem(time, value);
+    })
+    $("#currentDay").text(dayjs());
+});
